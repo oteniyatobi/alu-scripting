17f4d8646a546657b7feb6e86b1204949c742b57
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-06-28 20:28:39 +0200

    fixed  errors

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,11 +5,9 @@ import requests
 
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "linux:alu.api.advanced:v1.0 (by /u/oteniyatobi)"}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
