d5a6c2789660d05849bbee2f6611e42ccab3a72a
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-06-29 20:30:30 +0200

    Fix: handle non-200 status in top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "linux:alu.api.advanced:v1.0 (by /u/oteniyatobi)"}
     params = {"limit": 10}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
@@ -15,8 +15,7 @@ def top_ten(subreddit):
         return
     try:
         posts = response.json().get("data", {}).get("children", [])
+        for post in posts[:10]:
+            print(post.get("data", {}).get("title"))
     except ValueError:
         print(None)
-        return
-    for post in posts[:10]:
-        print(post.get("data", {}).get("title"))
